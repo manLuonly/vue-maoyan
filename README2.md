4a878292480b2cb7d4eaa2b717ab540fc15b6e50
Elmvx <elvmx47@aliyun.com>
2018-12-11 14:36:56 +0800

    首页布局is ok

---
+++ README2.md
@@ -0,0 +1,5 @@
+# 定位 html5 geoLocation
+
+- 百度地图 （http://lbsyun.baidu.com/）
+- 腾讯地图
+- 高德地图
--- fore-end/src/views/Films.vue
+++ fore-end/src/views/Films.vue
@@ -21,7 +21,98 @@
     </div>
     <!-- /轮播图 -->
 
-    <div style="height: 2000px;"></div>
+    <!-- 定位城市 -->
+    <div class="city-fixed">
+      <span>{{ curCity }}</span>
+      <i class="iconfont icon-xiala"></i>
+    </div>
+    <!-- /定位城市 -->
+
+    <!-- tab-bar -->
+    <div class="tab-bar-wrapper">
+      <ul class="tab-bar">
+        <li class="z-act">
+          <span>正在热映</span>
+        </li>
+        <li>
+          <span>即将上映</span>
+        </li>
+      </ul>
+    </div>
+    <!-- /tab-bar -->
+
+    <!-- list -->
+    <div class="film-list-content">
+      <ul>
+        <li>
+          <div class="img">
+            <img src="../images/film-1.jpg" alt="">
+          </div>
+          <div class="info">
+            <div>
+              <span class="name">海王</span>
+              <span class="type">3D</span>
+            </div>
+            <div>
+              <span class="label">观众评分</span>
+              <span class="grade">7</span>
+            </div>
+            <div>
+              <span class="label">主演： 帕特里克·威尔森 妮可·基德曼 杜夫·龙格尔 温子仁 杰森·莫玛 安柏·赫德</span>
+            </div>
+            <div>
+              <span class="label">美国 澳大利亚 | 143分钟</span>
+            </div>
+          </div>
+          <div class="buy">购票</div>
+        </li>
+        <li>
+          <div class="img">
+            <img src="../images/film-1.jpg" alt="">
+          </div>
+          <div class="info">
+            <div>
+              <span class="name">海王</span>
+              <span class="type">3D</span>
+            </div>
+            <div>
+              <span class="label">观众评分</span>
+              <span class="grade">7</span>
+            </div>
+            <div>
+              <span class="label">主演： 帕特里克·威尔森 妮可·基德曼 杜夫·龙格尔 温子仁 杰森·莫玛 安柏·赫德</span>
+            </div>
+            <div>
+              <span class="label">美国 澳大利亚 | 143分钟</span>
+            </div>
+          </div>
+          <div class="buy">购票</div>
+        </li>
+        <li>
+          <div class="img">
+            <img src="../images/film-1.jpg" alt="">
+          </div>
+          <div class="info">
+            <div>
+              <span class="name">海王</span>
+              <span class="type">3D</span>
+            </div>
+            <div>
+              <span class="label">观众评分</span>
+              <span class="grade">7</span>
+            </div>
+            <div>
+              <span class="label">主演： 帕特里克·威尔森 妮可·基德曼 杜夫·龙格尔 温子仁 杰森·莫玛 安柏·赫德</span>
+            </div>
+            <div>
+              <span class="label">美国 澳大利亚 | 143分钟</span>
+            </div>
+          </div>
+          <div class="buy">购票</div>
+        </li>
+      </ul>
+    </div>
+    <!-- /list -->
   </div>
 </template>
 
@@ -30,6 +121,22 @@
 import Swiper from 'swiper';
 export default {
   name: 'Films',
+
+  data () {
+    return {
+      // 当前城市
+      curCity: ''
+    }
+  },
+
+  created () {
+    // 获取当前的城市名称
+    /* eslint-disable */
+    let myCity = new BMap.LocalCity();
+    myCity.get((result) => {
+      this.curCity = result.name;
+    })
+  },
 
   mounted () {
     new Swiper('.swiper-container', {
@@ -65,6 +172,108 @@ export default {
   }
   .swiper-pagination-bullet-active {
     background: yellowgreen;
+  }
+}
+
+.city-fixed {
+  position: absolute;
+  top: px2rem(18);
+  left: px2rem(8);
+  z-index: 999;
+  height: px2rem(30);
+  line-height: px2rem(30);
+  font-size: px2rem(14);
+  color: #fff;
+  border-radius: px2rem(15);
+  text-align: center;
+  padding: 0 px2rem(5);
+  background: rgba(0, 0, 0, 0.2);
+}
+
+.tab-bar-wrapper {
+  position: sticky;
+  z-index: 999;
+  top: px2rem(0);
+  height: px2rem(50);
+  border-bottom: px2rem(1) solid #ededed;
+  background: #fff;
+  .tab-bar {
+    display: flex;
+    height: px2rem(50);
+    align-items: center;
+    li {
+      flex: 1;
+      text-align: center;
+      font-size: px2rem(14);
+      span {
+        padding: 0 px2rem(10);
+      }
+      &.z-act {
+        color: #ff5f16;
+      }
+    }
+  }
+}
+
+.film-list-content {
+  li {
+    display: flex;
+    margin: 0 px2rem(15);
+    padding: px2rem(15) 0;
+    border-bottom: px2rem(1) solid #ededed;
+  }
+
+  .img {
+    flex-shrink: 0;
+    width: px2rem(66);
+    height: px2rem(94);
+    img {
+      width: 100%;
+    }
+  }
+
+  .info {
+    min-width: 100px;
+    padding: 0 px2rem(10);
+    font-size: px2rem(14);
+    &>div {
+      width: 100%;
+      height: px2rem(22);
+      line-height: px2rem(22);
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .label {
+      color: #797d82;
+      margin-right: px2rem(2);
+    }
+    .name {
+      font-size: px2rem(16);
+      color: #191a1b;
+    }
+    .type {
+      font-size: px2rem(12);
+      color: #fff;
+      background: #d2d6dc;
+      padding: 0 px2rem(2);
+    }
+    .grade {
+      color: #ffb232;
+    }
+  }
+
+  .buy {
+    flex-shrink: 0;
+    align-self: center;
+    width: px2rem(50);
+    height: px2rem(26);
+    line-height: px2rem(26);
+    font-size: px2rem(14);
+    color: #ff5f16;
+    border: px2rem(1) solid #ff5f16;
+    text-align: center;
+    border-radius: px2rem(4);
   }
 }
 </style>
