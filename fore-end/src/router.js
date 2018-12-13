e1b0e88b95115a4d60e57a523dca41bcaee4dea6
Elmvx <elvmx47@aliyun.com>
2018-12-13 16:20:15 +0800

    路由拦截

--- fore-end/src/router.js
+++ fore-end/src/router.js
@@ -5,13 +5,12 @@ import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
 // 引入路由组件
-import Home from './views/Home.vue';
-import Films from './views/Films.vue';
-import NowPaly from './components/NowPaly';
-import SoonPlay from './components/SoonPlay';
-import Cinema from './views/Cinema.vue';
-import Center from './views/Center.vue';
-import FilmDetail from './views/FilmDetail.vue';
+// import Films from './views/Films.vue';
+// import NowPaly from './components/NowPaly';
+// import SoonPlay from './components/SoonPlay';
+// import Cinema from './views/Cinema.vue';
+// import Center from './views/Center.vue';
+// import FilmDetail from './views/FilmDetail.vue';
 
 // 插件安装
 Vue.use(VueRouter);
@@ -20,7 +19,7 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      component: Home,
+      component: () => import('./views/Home.vue'),
       children: [
         {
           path: '',
@@ -30,17 +29,17 @@ const router = new VueRouter({
           // 首页
           path: 'films',
           name: 'films',
-          component: Films,
+          component: () => import('./views/Films.vue'),
           children: [
             {
               path: 'nowPlaying',
               name: 'nowPlaying',
-              component: NowPaly
+              component: () => import('./components/NowPaly/index.vue')
             },
             {
               path: 'comingSoon',
               name: 'comingSoon',
-              component: SoonPlay
+              component: () => import('./components/SoonPlay/index.vue')
             }
           ]
         },
@@ -48,13 +47,13 @@ const router = new VueRouter({
           // 影院页
           path: 'cinemas',
           name: 'cinemas',
-          component: Cinema
+          component: () => import('./views/Cinema.vue')
         },
         {
           // 个人中心页
           path: 'center',
           name: 'center',
-          component: Center
+          component: () => import('./views/Center.vue')
         }
       ]
     },
@@ -62,11 +61,49 @@ const router = new VueRouter({
       // 详情页面
       path: '/film/:filmId',
       name: 'filmDetail',
-      component: FilmDetail,
+      component: () => import('./views/FilmDetail.vue'),
       beforeEnter (to, from, next) {
         console.log('我是一个路由独享的钩子函数');
         next();
       }
+    },
+    {
+      // 用户
+      path: '/user',
+      component: {
+        template: `
+          <div>
+            <router-view></router-view>
+          </div>
+        `
+      },
+      children: [
+        {
+          path: 'card',
+          component: () => import(/* webpackChunkName: "card" */ './views/Card.vue'),
+          beforeEnter (to, from, next) {
+            // 用没有登录
+            if (localStorage.getItem('userName')) {
+              next();
+            } else {
+              // 注意，如果需要实现，拦截到登陆页面之后，登录成功回跳到那个页面。
+              // localStorage.setItem('myNeedPage', '/user/card');
+              // next('/user/login');
+              console.log(to.fullPath);
+              next({
+                path: '/user/login',
+                query: {
+                  redirect: to.fullPath
+                }
+              })
+            }
+          }
+        },
+        {
+          path: 'login',
+          component: () => import('./views/Login.vue')
+        }
+      ]
     },
     {
       path: '*',
@@ -83,6 +120,14 @@ const router = new VueRouter({
  *  */
 router.beforeEach((to, from, next) => {
   NProgress.start();
+
+  // 判断当前要去的路由是不是卖座卡或者余额或者设置  这样在全局里面是没有问题，只是要做判断
+  // if (to.name === 'card' || to.name === 'yue' || to.name === 'set') {
+  //   // 如果当前用户有登录，就可以去
+  //   next();
+  //   // 如果没有，就让去登陆页面
+  //   next('/user/login')
+  // }
 
   console.log(to);
   console.log(from);
